1780716d2574dfffca5c881021b90f7076de9f5c
Daksh <GurenMarkV@users.noreply.github.com>
2019-01-16 01:20:22 -0500

    Add files via upload

--- Deck.py
+++ Deck.py
@@ -2,11 +2,21 @@ import random
 
 class Deck(object):
 
-    def generate_cards(self):
-        suits = ["Hearts", "Clubs", "Spades", "Diamonds"]
+	def __init__(self): #initialize card function into list_cards and add into draw_card
+		self.list_cards = self.generate_cards()
+
+	def draw_card(self):
+		return self.list_cards.pop()
+
+	def size(self):	#length of
+		return len(self.list_cards)
+	
+	#generate proper 52 card deck with 2 jokers added on
+	def generate_cards(self):
+		suits = ["Hearts", "Clubs", "Spades", "Diamonds"]
 		names = [ ("Three", 3), ("Four", 4), ("Five", 5), ("Six", 6), ("Seven", 7), 
 		("Eight", 8), ("Nine", 9), ("Ten", 10), ("Jack", 11), ("Queen", 12),  
-        ("King", 13), ("Ace", 14), ("Two", 15),  ("Joker", 16)]
+		("King", 13), ("Ace", 14), ("Two", 15), ("Joker", 16)]
 		cards = []
 		for name in names:
 			if name[0] == "Joker":
@@ -19,6 +29,7 @@ class Deck(object):
 				cards.append(card)
 		random.shuffle(cards)
 		return cards
+
 
 class Card(object):
 
@@ -38,4 +49,6 @@ class Card(object):
 
 
 
-
+# d = Deck()
+# print(d.generate_cards)
+# print(d.list_cards)
